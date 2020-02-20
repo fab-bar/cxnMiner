6e026951e74b793c3ce68fef8c829dede568e03f
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2020-02-20 12:51:16 +0100

    Add sn-gram extractor

---
+++ cxnminer/extractor.py
@@ -0,0 +1,68 @@
+import abc
+import itertools
+
+import conllu
+
+from .pattern import TokenSNGram, SNGram
+
+
+class PatternExtractor(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def extract_patterns(self, sentence):
+        pass
+
+class SyntacticNGramExtractor(PatternExtractor):
+    """Extracts non-continous sn-grams from a given sentence that are bottom-up subtrees."""
+
+    def __init__(self, min_size=2, max_size=6,
+                 special_node_conversion=None):
+
+        self.min_size = min_size
+        self.max_size = max_size
+        self.special_node_conversion = special_node_conversion
+
+    def _add_path(self, path, still_open_paths, patterns):
+
+        still_open_paths.append(path.tree)
+        if path.length >= self.min_size and path.length <= self.max_size:
+            patterns.append(path)
+
+    def _get_bottom_up_subtrees(self, tree):
+
+        still_open_paths = []
+        open_paths = []
+        patterns = []
+
+        if not tree.children:
+
+            self._add_path(TokenSNGram(tree), still_open_paths, patterns)
+
+            if self.special_node_conversion is not None:
+                special_path = self.special_node_conversion(tree)
+                if special_path is not None:
+                    self._add_path(TokenSNGram(special_path), still_open_paths, patterns)
+        else:
+            for child in tree.children:
+
+                local_paths, local_patterns = self._get_bottom_up_subtrees(child)
+
+                patterns.extend(local_patterns)
+                open_paths.append(local_paths)
+
+            still_open_paths = []
+            for open_path_iter in itertools.product(*open_paths):
+                open_path = TokenSNGram(SNGram.Tree(tree.token, open_path_iter))
+                self._add_path(open_path, still_open_paths, patterns)
+
+                if self.special_node_conversion is not None:
+                    special_path = self.special_node_conversion(tree)
+                    if special_path is not None:
+                        self._add_path(TokenSNGram(special_path), still_open_paths, patterns)
+
+        return still_open_paths, patterns
+
+    def extract_patterns(self, sentence):
+
+        return self._get_bottom_up_subtrees(sentence.to_tree())[1]
+
---
+++ tests/test_extractor.py
@@ -0,0 +1,212 @@
+import pytest
+from pytest_cases import cases_generator, cases_data, THIS_MODULE
+
+import conllu
+
+from cxnminer.extractor import SyntacticNGramExtractor
+from cxnminer.pattern import SNGram
+
+test_sentences = conllu.parse(
+"""
+# text = The quick brown fox jumps over the lazy dog.
+1   The     the    DET    DT   Definite=Def|PronType=Art   4   det     _   _
+2   quick   quick  ADJ    JJ   Degree=Pos                  4   amod    _   _
+3   brown   brown  ADJ    JJ   Degree=Pos                  4   amod    _   _
+4   fox     fox    NOUN   NN   Number=Sing                 5   nsubj   _   _
+5   jumps   jump   VERB   VBZ  Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin   0   root    _   _
+6   over    over   ADP    IN   _                           9   case    _   _
+7   the     the    DET    DT   Definite=Def|PronType=Art   9   det     _   _
+8   lazy    lazy   ADJ    JJ   Degree=Pos                  9   amod    _   _
+9   dog     dog    NOUN   NN   Number=Sing                 5   nmod    _   SpaceAfter=No
+10  .       .      PUNCT  .    _                           5   punct   _   _
+
+# text = Foxes jump over the lazy dog.
+1   Foxes     fox    NOUN   NN   Number=Plur                 2   nsubj   _   _
+2   jump   jump   VERB   VBZ  Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin   0   root    _   _
+3   over    over   ADP    IN   _                           6   case    _   _
+4   the     the    DET    DT   Definite=Def|PronType=Art   6   det     _   _
+5   lazy    lazy   ADJ    JJ   Degree=Pos                  6   amod    _   _
+6   dog     dog    NOUN   NN   Number=Sing                 2   nmod    _   SpaceAfter=No
+7  .       .      PUNCT  .    _                           2   punct   _   _
+"""
+)
+
+test_data = {
+    "The quick brown fox jumps over the lazy dog.": {
+        "1_3_None": {
+            "ngrams": set([
+                "The",
+                "quick",
+                "brown",
+                "over",
+                "the",
+                "lazy",
+                ".",
+            ])
+        },
+        "1_3_conversion_function": {
+            "ngrams": set([
+                "The",
+                "quick",
+                "brown",
+                "nsubj",
+                "over",
+                "the",
+                "lazy",
+                "nmod",
+                ".",
+            ])
+        },
+        "2_3_None": {
+            "ngrams": set([]),
+        },
+        "2_3_conversion_function": {
+            "ngrams": set([]),
+        },
+        "2_4_None": {
+            "ngrams": set([
+                "fox [The, quick, brown]",
+                "dog [over, the, lazy]",
+            ]),
+        },
+        "2_4_conversion_function": {
+            "ngrams": set([
+                "fox [The, quick, brown]",
+                "dog [over, the, lazy]",
+                "jumps [nsubj, nmod, .]",
+            ]),
+        },
+        "2_9_None": {
+            "ngrams": set([
+                "fox [The, quick, brown]",
+                "dog [over, the, lazy]",
+            ]),
+        },
+        "2_9_conversion_function": {
+            "ngrams": set([
+                "fox [The, quick, brown]",
+                "dog [over, the, lazy]",
+                "jumps [fox [The, quick, brown], nmod, .]",
+                "jumps [nsubj, dog [over, the, lazy], .]",
+                "jumps [nsubj, nmod, .]",
+            ])
+        },
+        "2_10_None": {
+            "ngrams": set([
+                "fox [The, quick, brown]",
+                "dog [over, the, lazy]",
+                "jumps [fox [The, quick, brown], dog [over, the, lazy], .]",
+            ]),
+        },
+        "2_10_conversion_function": {
+            "ngrams": set([
+                "fox [The, quick, brown]",
+                "dog [over, the, lazy]",
+                "jumps [fox [The, quick, brown], dog [over, the, lazy], .]",
+                "jumps [fox [The, quick, brown], nmod, .]",
+                "jumps [nsubj, dog [over, the, lazy], .]",
+                "jumps [nsubj, nmod, .]",
+            ]),
+        },
+    },
+    "Foxes jump over the lazy dog.": {
+        "1_3_None": {
+            "ngrams": set([
+                "Foxes",
+                "over",
+                "the",
+                "lazy",
+                ".",
+            ])
+        },
+        "1_3_conversion_function": {
+            "ngrams": set([
+                "Foxes",
+                "nsubj",
+                "over",
+                "the",
+                "lazy",
+                "nmod",
+                ".",
+            ])
+        },
+        "2_3_None": {
+            "ngrams": set([]),
+        },
+        "2_3_conversion_function": {
+            "ngrams": set([]),
+        },
+        "2_4_None": {
+            "ngrams": set([
+                "dog [over, the, lazy]",
+            ]),
+        },
+        "2_4_conversion_function": {
+            "ngrams": set([
+                "dog [over, the, lazy]",
+                "jump [Foxes, nmod, .]",
+                "jump [nsubj, nmod, .]",
+            ]),
+        },
+        "2_9_None": {
+            "ngrams": set([
+                "dog [over, the, lazy]",
+                "jump [Foxes, dog [over, the, lazy], .]",
+            ]),
+        },
+        "2_9_conversion_function": {
+            "ngrams": set([
+                "dog [over, the, lazy]",
+                "jump [Foxes, nmod, .]",
+                "jump [nsubj, dog [over, the, lazy], .]",
+                "jump [Foxes, dog [over, the, lazy], .]",
+                "jump [nsubj, nmod, .]",
+            ])
+        },
+        "2_10_None": {
+            "ngrams": set([
+                "dog [over, the, lazy]",
+                "jump [Foxes, dog [over, the, lazy], .]",
+            ]),
+        },
+        "2_10_conversion_function": {
+            "ngrams": set([
+                "dog [over, the, lazy]",
+                "jump [Foxes, nmod, .]",
+                "jump [nsubj, dog [over, the, lazy], .]",
+                "jump [Foxes, dog [over, the, lazy], .]",
+                "jump [nsubj, nmod, .]",
+            ]),
+        },
+    }
+}
+
+
+
+def conversion_function(tree):
+
+    if tree.token['upostag'] == "NOUN":
+        return SNGram.Tree(dict(tree.token, **{'form': tree.token['deprel']}), [])
+    else:
+        return None
+
+@cases_generator("sentence={sentence.metadata[text]} min={min_max[0]}, max={min_max[1]}, conversion={conversion}",
+                 sentence=test_sentences,
+                 min_max=[(1,3), (2,3), (2,4), (2,9), (2,10)],
+                 conversion=[None, conversion_function],
+)
+def case_simple_generator(sentence, min_max, conversion):
+
+    return (
+        sentence,
+        SyntacticNGramExtractor(
+            min_size=min_max[0], max_size=min_max[1], special_node_conversion=conversion),
+        test_data[sentence.metadata['text']]["_".join([str(min_max[0]), str(min_max[1]), getattr(conversion, '__name__', 'None')])]
+    )
+
+
+@cases_data(module=THIS_MODULE)
+def test_extract_ngrams_elements(case_data):
+
+    sentence, extractor, expected = case_data.get()
+    assert set([str(pattern.get_pattern_list(['form'])[0]) for pattern in extractor.extract_patterns(sentence)]) == expected['ngrams']
