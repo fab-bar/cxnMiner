4b15cb460ab2e7239b8796acfa90dab8e1049aaf
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2020-03-13 14:19:27 +0100

    Improve open_file

--- cxnminer/utils/helpers.py
+++ cxnminer/utils/helpers.py
@@ -3,6 +3,17 @@ import gzip
 def open_file(filename, mode='r', encoding='utf-8'):
 
     if filename.endswith(".gz"):
-        return gzip.open(filename, mode + 't', encoding=encoding)
+
+        ## gzip opens in binary mode by default
+        ## assure text mode if binary mode is not explicitely requested
+        if 'b' not in mode and 't' not in mode:
+            return gzip.open(filename, mode + 't', encoding=encoding)
+        else:
+            return gzip.open(filename, mode)
+
     else:
-        return open(filename, mode, encoding=encoding)
+
+        if 'b' not in mode:
+            return open(filename, mode, encoding=encoding)
+        else:
+            return open(filename, mode)
