50b6737ee65aa9c57005c1659b7086f965442a87
fab-bar <fabian.barteld@ruhr-uni-bochum.de>
2020-03-16 17:28:05 +0100

    Add tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,17 +5,34 @@ import pytest
 from cxnminer.utils.helpers import open_file
 
 @mock.patch('builtins.open')
-def test_open_file(mockfunction):
+def test_open_text_file(mockfunction):
 
     filename = 'test.txt'
 
     open_file(filename)
     mockfunction.assert_called_with(filename, 'r', encoding='utf-8')
 
+@mock.patch('builtins.open')
+def test_open_binary_file(mockfunction):
+
+    filename = 'test.bin'
+
+    open_file(filename, 'rb')
+    mockfunction.assert_called_with(filename, 'rb')
+
 @mock.patch('gzip.open')
-def test_open_zipped_file(mockfunction):
+def test_open_zipped_text_file(mockfunction):
 
     filename = 'test.txt.gz'
 
     open_file(filename)
     mockfunction.assert_called_with(filename, 'rt', encoding='utf-8')
+
+
+@mock.patch('gzip.open')
+def test_open_zipped_binary_file(mockfunction):
+
+    filename = 'test.bin.gz'
+
+    open_file(filename, 'rb')
+    mockfunction.assert_called_with(filename, 'rb')
